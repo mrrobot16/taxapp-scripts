f26099ec2360a281b08a786269e418bc75ec3f8e
Hector G <hmgarcia06@gmail.com>
2023-12-04 00:07:00 -0500

    Added a constants file

---
+++ scripts/constants/__init__.py
@@ -0,0 +1,45 @@
+role = "user"
+# content = ("My client is single, currently employed by Company A as well as consulting "
+#            "contract for Company C. Bought X amount of stocks at Company D at price Y and "
+#            "sold such Company C stock the same year for Z price. X dollars where collected "
+#            "through saving accounts interest. Company stock F distributed X dollars in dividends. "
+#            "Client is shareholder of a s-corporation and c-corporation that had a net income and "
+#            "distributed dividends. Client owns a few properties that are currently rented through "
+#            "a property manager. Some of the propertities had an amortization and depreciation. "
+#            "One of the properties is under a mortgage with around 5% interest rate. Earlier this "
+#            "year one of the properties suffered damages from a natural disaster and had to do repairs, "
+#            "purchase new furnitures and appliances. What forms and urls of those forms do i need to "
+#            "file for my client?")
+content = "who are you?"
+user_requests = [
+	{
+		"user_id": "043871ab-29d3-40b7-8",
+		"conversation_id": "21101597-9875-4e1c-9",
+        "content": content,
+        "role": role
+	},
+	{
+		"user_id": "1f765abe-cc9d-44bd-8",
+		"conversation_id": "f289da13-d8d7-4eb9-a",
+        "content": content,
+        "role": role
+	},
+	{
+		"user_id": "adfce3da-cebb-484f-9",
+		"conversation_id": "759c9cf1-840f-4a60-9",
+        "content": content,
+        "role": role
+	},
+	{
+		"user_id": "af3d633f-e654-44bf-a",
+		"conversation_id": "efce74b7-d50e-4d78-a",
+        "content": content,
+        "role": role
+	},
+	{
+		"user_id": "b688727c-790f-4992-8",
+		"conversation_id": "b70b8eb7-ab96-4bd8-a",
+        "content": content,
+        "role": role
+	},
+]
