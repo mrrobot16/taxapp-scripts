5af4e9e2e9d0cb243c6e7459bf6bfbc017ad9460
Hector G <hmgarcia06@gmail.com>
2023-12-04 00:07:26 -0500

    refactored outputs folder logic and script to make calls to chat-completion

--- scripts/taxapp-api.py
+++ scripts/taxapp-api.py
@@ -5,88 +5,53 @@ import typer
 import uuid
 import time
 import os
+from datetime import datetime
+
+from constants import user_requests
 
 app = typer.Typer()
 
-user_id = "b688727c-790f-4992-8"
-conversation_id = "b70b8eb7-ab96-4bd8-a"
-role = "user"
-# content = ("My client is single, currently employed by Company A as well as consulting "
-#            "contract for Company C. Bought X amount of stocks at Company D at price Y and "
-#            "sold such Company C stock the same year for Z price. X dollars where collected "
-#            "through saving accounts interest. Company stock F distributed X dollars in dividends. "
-#            "Client is shareholder of a s-corporation and c-corporation that had a net income and "
-#            "distributed dividends. Client owns a few properties that are currently rented through "
-#            "a property manager. Some of the propertities had an amortization and depreciation. "
-#            "One of the properties is under a mortgage with around 5% interest rate. Earlier this "
-#            "year one of the properties suffered damages from a natural disaster and had to do repairs, "
-#            "purchase new furnitures and appliances. What forms and urls of those forms do i need to "
-#            "file for my client?")
-content = "who are you?"
-
-request_data = {
-    "user_id": user_id,
-    "conversation_id": conversation_id,
-    "content": content,
-    "role": role
-}
-output_folder = "taxapp-api-outputs"
-
-os.makedirs(output_folder, exist_ok=True)
-
 async def make_request(client, url, data):
-    response = await client.post(url, json=request_data)
+    response = await client.post(url, json=data)
     return data, response.json()
 
 def save_to_file(request_response_pair):
-    print('request_response_pair', request_response_pair)
-    filename = f"{uuid.uuid4()}.txt"
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    output_folder = f"taxapp-api-outputs/{timestamp}"
+    os.makedirs(output_folder, exist_ok=True)
+    unique = str(uuid.uuid4())
+    filename = f"{unique[:8]}.txt"
     filepath = os.path.join(output_folder, filename)
     with open(filepath, 'w') as file:
-        # content = {
-        #     "request": request_response_pair[0],
-        #     "response": request_response_pair[1]
-        # }
-        content = request_response_pair
+        content = {
+            "request": request_response_pair[0],
+            "response": request_response_pair[1]
+        }
         file.write(json.dumps(content, indent=4))
+
+async def main(url, num_requests):
+    async with httpx.AsyncClient(timeout=60) as client:
+        tasks = []
+        for i in range(num_requests):
+            for user_request in user_requests:  # Making specified number of requests
+                data = user_request
+                task = await make_request(client, url, data)
+                tasks.append(task)
+        for task in tasks:
+            save_to_file(task)
 
 @app.command()
 def run_requests(url: str, num_requests: int):
+    start_datetime = datetime.now()
+    print("Start datetime:", start_datetime)
     start_time = time.time()
     asyncio.run(main(url, num_requests))
     end_time = time.time()
     print(f"Total execution time: {end_time - start_time:.2f} seconds")
 
-
-async def main(url, num_requests):
-    async with httpx.AsyncClient(timeout=60) as client:
-        tasks = []
-        for i in range(num_requests):  # Making specified number of requests
-            data = request_data
-            task = await make_request(client, url, data)
-            tasks.append(task)
-        print('\n')
-        print('len(tasks)', len(tasks))
-        print('\n')
-        # print('tasks', tasks)
-        for task in tasks:
-            save_to_file(task)
-        # request_response_pairs = await asyncio.gather(*tasks)
-        # print('request_response_pairs', request_response_pairs)
-
-        # for pair in request_response_pairs:
-        #     await save_to_file(pair)
-
 @app.command()
-def hello(name: str, num_requests: int):
-    print(f"Hello {name}")
-
-@app.command()
-def goodbye(name: str, formal: bool = False):
-    if formal:
-        print(f"Goodbye Ms. {name}. Have a good day.")
-    else:
-        print(f"Bye {name}!")
+def placeholder(name: str):
+    pass
 
 if __name__ == "__main__":
     app()
